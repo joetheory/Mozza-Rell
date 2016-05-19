931e4aa27b2864c76a5df463f0dba2ef82afcf01
joetheory <joetheory@gmail.com>
2016-05-19 16:03:09 -0400

    Connected

--- mozza-rell.js
+++ mozza-rell.js
@@ -1,16 +1,43 @@
 var Discordie = require("discordie");
-var client = new Discordie();
+var Discord = new Discordie();
 
 var Config = require("./config.json");
 var token = Config.token;
-client.connect({ token: token });
+Discord.connect({ token: token });
 
-client.Dispatcher.on("GATEWAY_READY", e => {
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         'cyC7rxylZPlXNwSj7EHt7MZUW',
+  consumer_secret:      Config.twitSecret,
+  access_token:         '486675175-tSMCerO8D0yDLuocZX1JXkmEqrlDY6iHJcCnV0Oe',
+  access_token_secret:  Config.twitTokenSecret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+
+
+
+var stream = T.stream('statuses/filter', { follow: '486675175,14119066,809487942' });
+
+stream.on('tweet', function (tweet) {
+	if(tweet.retweeted === false) {
+		console.log(tweet);
+		var guild = Discord.Guilds.find(g => g.name == "DevTayne");
+		channel = guild.textChannels.find(c => c.name == "general");
+		channel.sendMessage('@' + tweet.user.screen_name + ' tweeted: ' + tweet.text );
+	} 
+	console.log(tweet);
+})
+
+Discord.Dispatcher.on("GATEWAY_READY", e => {
+	Discord.User.setUsername('Mozza-Rell');
 	console.log("New Event: GATEWAY_READY");
-	console.log("Hey, I'm " + client.User.username + ", your latest dancer. I can't wait to entertain you.");
+	console.log("Hey, I'm " + Discord.User.username + ", your latest dancer. I can't wait to entertain you.");
+	
 });
 
-client.Dispatcher.on("MESSAGE_CREATE", e => {
+Discord.Dispatcher.on("MESSAGE_CREATE", e => {
 	console.log("New Event: MESSAGE_CREATE");
 	console.log(">(" + e.message.timestamp + ") " + e.message.author.username + ": " + e.message.content);
 
